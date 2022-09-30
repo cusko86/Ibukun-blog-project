7a0f26b1cdf37f8fa868bc4638a96a8a49a37635
Ibukun <adewumibukun@gmail.com>
2022-09-30 21:28:48 +0100

    postgres

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 secret_key = os.getenv("SECRET_KEY")
 if secret_key.startswith("postgres://"):
     secret_key = secret_key.replace("postgres://", "postgresql://", 1)
@@ -22,10 +22,10 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
